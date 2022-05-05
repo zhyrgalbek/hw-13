164720cfbf23fdf4460516aa2b7ea191d7a4c1fa
zhyrgalbek <zhyrgalbek.kamalov@gmail.com>
2022-05-05 14:58:08 +0300

    add ramka id

--- src/Components/Post/PostItem.js
+++ src/Components/Post/PostItem.js
@@ -2,7 +2,7 @@ import './PostItem.css';
 
 function PostItem(props){
     return <li className="item">
-        <h2 className='id'>{props.id}</h2>
+        <h2 className='id'><span>{props.id}</span></h2>
         <p>{props.title}</p>
         <img src={props.url} />
     </li>
